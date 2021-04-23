65027323590f91d14b5a0b7a5e3fce95e1b155db
Speak2Erase <matthew@nowaffles.com>
2021-04-23 08:38:57 -0700

    Update make-linux-dist.sh
    for some reason i can't compile without this being sudo so i need to modify this apparently

--- make-linux-dist.sh
+++ make-linux-dist.sh
@@ -21,9 +21,9 @@ function copy_dependencies() {
     [[ ! $1 =~ ^[a-zA-Z0-9+\.-]*$ ]] && fail "The library $1 has weird characters!"
 
     SO_PROCESSED="$SO_PROCESSED $1"
-    cp "$2" "$DESTDIR/$1"
-    patchelf --set-rpath '$ORIGIN' "$DESTDIR/$1"
-    ldd "$2" | while read -ra line; do
+    sudo cp "$2" "$DESTDIR/$1"
+    sudo patchelf --set-rpath '$ORIGIN' "$DESTDIR/$1"
+    sudo ldd "$2" | while read -ra line; do
         if [[ ${line[0]} == 'linux-vdso.so.1' ]] || [[ ${line[0]} =~ 'ld-linux-x86-64.so.2' ]]; then
             continue
         fi
@@ -41,17 +41,17 @@ shopt -s dotglob
 
 if [ $# -eq 3 ] && [ -d $3 ]; then
     echo "Copying game files..."
-    cp -ar $3/* $2
+    sudo cp -ar $3/* $2
 fi
 
 echo "Relocating dependencies..."
 DESTDIR="$2/lib"
-mkdir -p $DESTDIR
+sudo mkdir -p $DESTDIR
 copy_dependencies oneshot "$1/bin/oneshot"
 
 echo "Copying standard library..."
-cp -ar "$1/bin/lib/ruby" "$2/lib/"
-cp -ar "$1/bin/lib/cacert.pem" "$2/lib/"
-ln -sf "lib/oneshot" "$2/oneshot"
+sudo cp -ar "$1/bin/lib/ruby" "$2/lib/"
+sudo cp -ar "$1/bin/lib/cacert.pem" "$2/lib/"
+sudo ln -sf "lib/oneshot" "$2/oneshot"
 
 echo "Done!"
